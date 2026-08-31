630c8eb61e3f170ee85a131a412bad392e7f04cf
Bebek Takip Asistan <bebek-takip@local>
2026-08-31 14:50:55 +0000

    DUZELTME: careTypes/clips durumu eksik kalmisti - Bakim ve Sesler sekmeleri onarildi

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bebek-takip-v21';
+const CACHE = 'bebek-takip-v22';
 const SHELL = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', (event) => {
